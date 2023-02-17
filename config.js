c965e84103d01ffafdc3dbae440095bb275cf51d
Leighton <LeightonShervington@gmail.com>
2023-02-16 22:43:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
 	name: 'Leighton',
 	imageBackground: true,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
